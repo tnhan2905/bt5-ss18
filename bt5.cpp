0f1eb72b833d3657e00c33e79baa31986e68273c
tnhan2905 <diepthanhnhan2019.st@gmail.com>
2025-01-09 07:54:16 +0700

    Add files via upload

--- bt5.cpp
+++ bt5.cpp
@@ -1,25 +1,68 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define max 50
 
-void changeIndex(int a[],int *newValue,int *changeIndex);
-void inMang(int a[],int *n);
+struct sinhVien{
+	int id;
+	char name[20];
+	int age;
+	char phoneNum[20];
+};
+
+typedef struct sinhVien sinhVien;
+void showSVList(sinhVien arr[],int sizeSVList);
+void fixIFSV(sinhVien arr[],int sizeSVList);
 
 int main(){
-	int n=5;
-	int a[n] = {2,4,6,8,10};
-	printf("Mang truoc khi cap nhat: ");
-	inMang(a,&n);
-	int newValue=99,Index=2;
-	changeIndex(a,&newValue,&Index);
-	printf("\nMang sau khi cap nhat: ");
-	inMang(a,&n);
+	int sizeSVList=5;
+	sinhVien arr[max] = {
+		{1,"Le Thanh Hien", 21,"0987654321"},
+		{2,"Tran Thanh Tam",27,"0987654321"},
+		{3,"Nguyen Van Tu",32,"0987654321"},
+		{4,"Thai Thanh Hao",60,"0987654321"},
+		{5,"Tran Thanh",50,"0987654321"},
+	};
+	
+	printf("Danh sach sinh vien truoc khi sua:\n");
+	showSVList(arr,sizeSVList);
+	
+	fixIFSV(arr,sizeSVList);
+	
+	printf("Danh sach sinh vien sau khi sua:\n");
+	showSVList(arr,sizeSVList);
 }
 
-void changeIndex(int a[],int *newValue,int *Index){
-	a[*Index-1] = *newValue;
-}
-
-void inMang(int a[],int *n){
-	for(int i=0;i<*n;i++){
-		printf("%d ",*(a+i));
+void showSVList(sinhVien arr[],int sizeSVList){
+	printf("==============================================\n");
+	printf("|%-5s%-20s%-5s%-15s|\n","ID","Name","Age","Phone Number");
+	printf("=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+\n");
+	for(int i=0;i<sizeSVList;i++){
+		printf("|%-5d%-20s%-5d%-15s|\n",arr[i].id,arr[i].name,arr[i].age,arr[i].phoneNum);
+		printf("=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+\n");
 	}
+}
+
+void fixIFSV(sinhVien arr[],int sizeSVList){
+	int fixID;
+	printf("Nhap vao ID sinh vien ban can sua: ");	
+	scanf("%d",&fixID);
+	fflush(stdin);
+	
+	for(int i=0;i<sizeSVList;i++){
+		if(arr[i].id == fixID){
+			sinhVien fixIF;
+			fixIF = arr[i];
+			printf("Nhap vao ten sinh vien can sua: "); 
+			gets(fixIF.name);
+			printf("Nhap vao tuoi sinh vien can sua: ");	
+			scanf("%d",&fixIF.age);
+			fflush(stdin);
+			
+			arr[i] = fixIF;
+			return;
+		}
+	}
+	
+	printf("ID sinh vien khong ton tai!");
 }
